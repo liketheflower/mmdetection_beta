1b781aed14cc293b4b697b6633c0678a2bbf6044
jimmy shen <jimmy@takemobi.com>
2022-02-20 22:52:17 -0500

    Update vis output code

--- demo/vis_output_tensor.py
+++ demo/vis_output_tensor.py
@@ -2,14 +2,21 @@ import pandas as pd
 import numpy as np
 from PIL import Image as im
 
+import seaborn as sns
+import matplotlib.pylab as plt
 
-def normalize(a):
-    min_ = np.min(a)
-    max_ = np.max(a)
+
+def normalize(a, min_=None, max_=None, clip=False):
+    if min_ is None or max_ is None:
+        min_ = np.min(a)
+        max_ = np.max(a)
     new_a = (a - min_) / (max_ - min_)
+    if clip:
+        new_a = np.clip(new_a, 0, 1)
     return new_a
 
-outputs = pd.read_pickle("swin_outs.pkl")
+
+outputs = pd.read_pickle("/Users/jimmy/repos/jimmy/output_featuremap/swin_outs.pkl")
 outs = list(outputs["outs"])
 
 last_layer = outs[-1]
@@ -30,20 +37,42 @@ print(np.max(last_np))
 print(np.min(last_np))
 
 last_np_normalize = normalize(last_np)
-#last_np_normalize = last_np_normalize*255
-#last_np_normalize = last_np_normalize.dtype(np.uint8)
-output_img = np.ones((H*32, W*24), dtype=np.float)
-#output_img = output_img.dtype(np.uint8)
+# last_np_normalize = normalize(last_np_normalize, 0.57, 0.7)
+# last_np_normalize = last_np_normalize*255
+# last_np_normalize = last_np_normalize.dtype(np.uint8)
+output_img = np.ones((H * 32, W * 24), dtype=np.float)
+# output_img = output_img.dtype(np.uint8)
 idx = 0
 for i in range(32):
     for j in range(24):
-        output_img[i*H:i*H+H, j*W:j*W +W] = last_np[idx]
+        output_img[i * H : i * H + H, j * W : j * W + W] = last_np_normalize[idx]
         idx += 1
 
 
-import seaborn as sns
-import matplotlib.pylab as plt
+last_np_normalize_flat = last_np_normalize.ravel()
+plt.hist(last_np_normalize_flat, bins=200)
+plt.show()
+plt.close()
+# uniform_data = np.random.rand(10, 12)
+ax = sns.heatmap(output_img)
+plt.show()
+# plt.savefig("swin_heap.pdf", dpi=500)
+# remove outlier
+last_np_normalize = normalize(last_np_normalize, 0.57, 0.7, clip=True)
 
-#uniform_data = np.random.rand(10, 12)
-ax = sns.heatmap(output_img, linewidth=0.5)
-plt.savefig("swin_heap.pdf", dpi=500)
+output_img = np.ones((H * 32, W * 24), dtype=np.float)
+# output_img = output_img.dtype(np.uint8)
+idx = 0
+for i in range(32):
+    for j in range(24):
+        output_img[i * H : i * H + H, j * W : j * W + W] = last_np_normalize[idx]
+        idx += 1
+
+
+last_np_normalize_flat = last_np_normalize.ravel()
+plt.hist(last_np_normalize_flat, bins=200)
+plt.show()
+plt.close()
+# uniform_data = np.random.rand(10, 12)
+ax = sns.heatmap(output_img)
+plt.show()
