478fde498e2146ba8d0da84add6b9acda2ecbc00
jimmy shen <jimmy@takemobi.com>
2022-04-10 00:47:05 -0400

    Update log analysis code (#16)

--- tools/analysis_tools/run_analyze_YONOD_logs.sh
+++ tools/analysis_tools/run_analyze_YONOD_logs.sh
@@ -1,10 +1,8 @@
+<<comment
 json_logs="/Users/jimmy/repos/jimmy/YONOD/work_dirs/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco_RGB_load_from/20220211_002440.log.json  /Users/jimmy/repos/jimmy/YONOD/work_dirs/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_sunrgbd_rgb2dhs/20220212_073746.log.json" 
-#python analyze_logs.py cal_train_time ${json_logs}
-#python analyze_logs.py plot_curve ${json_logs} 
-#"loss_rpn_cls": 0.09454, "loss_rpn_bbox": 0.04825, "loss_cls": 0.48573, "acc": 90.14453, "loss_bbox": 0.35008, "loss_mask": 0.0, "loss": 0.9786, "time": 0.33392}
-#python analyze_logs.py plot_curve ${json_logs} --keys="loss_rpn_cls"
-#python analyze_logs.py plot_curve ${json_logs} --keys="loss_rpn_bbox"
-#python analyze_logs.py plot_curve ${json_logs} --keys="loss_cls"
-#python analyze_logs.py plot_curve ${json_logs} --keys="loss_bbox"
+python analyze_logs.py plot_curve ${json_logs} --keys="bbox_mAP_50"
+comment
+
+json_logs="/Users/jimmy/repos/jimmy/YONOD/work_dirs/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco_RGB_load_from/cc.log.json  /Users/jimmy/repos/jimmy/YONOD/work_dirs/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_sunrgbd_rgb2dhs/cc.log.json" 
 python analyze_logs.py plot_curve ${json_logs} --keys="bbox_mAP_50"
 
