b3e82b40deb56a25ad84926238261d73f605fca4
jimmy shen <jimmy@takemobi.com>
2022-06-23 23:47:05 -0400

    Add swin-s flops

--- tools/analysis_tools/run_get_flops_yonod.sh
+++ tools/analysis_tools/run_get_flops_yonod.sh
@@ -1,3 +1,4 @@
 # Swin -S
-cfg=/data/sophia/a/Xiaoke.Shen54/repos/SwinTransFusion/work_dirs/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_sunrgbd_rgbdhs_mixed/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_sunrgbd_rgbdhs_mixed.py
+#cfg=/data/sophia/a/Xiaoke.Shen54/repos/SwinTransFusion/work_dirs/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_sunrgbd_rgbdhs_mixed/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_sunrgbd_rgbdhs_mixed.py
+cfg=/Users/jimmy/repos/jimmy/YONOD/work_dirs/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_sunrgbd_rgbdhs_mixed/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_sunrgbd_rgbdhs_mixed.py
 python get_flops.py $cfg  > swin_s_flops.log 
