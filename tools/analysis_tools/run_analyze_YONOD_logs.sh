3f003a7f869b05941e463e5f34aaabc9081a5afc
jimmy shen <jimmy@takemobi.com>
2022-06-23 22:00:14 -0400

    Update YONOD analysis log (#18)

--- tools/analysis_tools/run_analyze_YONOD_logs.sh
+++ tools/analysis_tools/run_analyze_YONOD_logs.sh
@@ -2,7 +2,7 @@
 json_logs="/Users/jimmy/repos/jimmy/YONOD/work_dirs/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco_RGB_load_from/20220211_002440.log.json  /Users/jimmy/repos/jimmy/YONOD/work_dirs/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_sunrgbd_rgb2dhs/20220212_073746.log.json" 
 python analyze_logs.py plot_curve ${json_logs} --keys="bbox_mAP_50"
 
-json_logs="/Users/jimmy/repos/jimmy/YONOD/work_dirs/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco_RGB_load_from/cc.log.json  /Users/jimmy/repos/jimmy/YONOD/work_dirs/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_sunrgbd_rgb2dhs/cc.log.json" 
+json_logs="/Users/jimmy/repos/jimmy/YONOD/work_dirs/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco_RGB_load_from/cc.log.json  /Users/jimmy/repos/jimmy/YONOD/work_dirs/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_sunrgbd_rgb2dhs/cc.log.json /Users/jimmy/repos/jimmy/YONOD/work_dirs/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_sunrgbd_rgb_dhs_mixed/cc.log.json" 
 python analyze_logs.py plot_curve ${json_logs} --keys="bbox_mAP_50"
 
 comment
